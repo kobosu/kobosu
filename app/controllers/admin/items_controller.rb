88c0a659fb335aebff4fe8e7e70c23f00234b5d6
ichisei <132669382+ichisei@users.noreply.github.com>
2023-06-21 05:27:57 +0000

    re

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,19 +1,37 @@
 class ItemsController < ApplicationController
-  def index
-  end
 
   def new
+    @item = Item.new
+    # @genres = Genre.new
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+  end
+
+  def index
+    @items = Item.all
+    
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to item_path(item.id)
+    #リンク先確認・修正
   end
+
+  def item_params
+    params.require(:item).permit(:name, :tax_out_price, :explanation, :profile_image)
+  end
+
 end
