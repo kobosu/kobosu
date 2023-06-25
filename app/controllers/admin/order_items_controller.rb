d5c4a829253f3d7bda1c1eba20b7cbace5e528d5
ichisei <132669382+ichisei@users.noreply.github.com>
2023-06-25 11:51:23 +0000

    [Update6]public_orders

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,4 +1,12 @@
 class Admin::OrderItemsController < ApplicationController
+  
   def update
+    @order_items = CartItem.create(cart_items_params)
   end
+  
+  private
+  def cart_items_params
+    params.require(:cart_item).permit(:item_id, :quantity)
+  end
+
 end
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,6 +1,7 @@
 class Public::OrdersController < ApplicationController
   def new
      @order = Order.new
+     
   end
 
   def confilm
@@ -10,39 +11,42 @@ class Public::OrdersController < ApplicationController
 
     when 0 then
     @order = Order.new(order_params)
-    @order.postal_code = current_customer.postal_code
+    @order.postcode = current_customer.postcode
     @order.address = current_customer.address
     @order.name = current_customer.first_name + current_customer.last_name
 
     when 1 then
     @order = Order.new(order_params)
     @address = Shipping_address.find(params[:order][:address_id])
-    @order.postal_code = @address.postal_code
+    @order.postcode = @address.postcode
     @order.address = @address.address
     @order.name = @address.name
 
 
     when 2 then
     @shipping_address = ShippingAddress.new
-    @shipping_address.postal_code = params[:order][:postcode]
+    @shipping_address.postcode = params[:order][:postcode]
     @shipping_address.address = params[:order][:address]
     @shipping_address.name = params[:order][:name]
 
     @shipping_address.save
 
-    @order.postal_code = @shipping_address.postal_code
+    @order.postcode = @shipping_address.postcode
     @order.address = @shipping_address.address
     @order.name = @shipping_address.name
 
     end
-
     @cart_items = current_costomer.cart_items
+    @postage = 800
+    
+  end
+  
+  def create
+    @order = Order.new(order_params)
+    @order.save
   end
 
   def complete
-  end
-
-  def create
   end
 
   def index
@@ -52,10 +56,8 @@ class Public::OrdersController < ApplicationController
   end
 
   private
-def order_params
-  params.require(:order).permit(:payment_method, :postal_code, :address, :name)
-
-end
-
+  def order_params
+    params.require(:order).permit(:payment_method, :billing_amount, :postcode, :address, :name, :postage)
+  end
 
 end
