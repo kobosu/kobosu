6fea616341555e50bf32a617474ecd1307602e9f
EC2 Default User <ec2-user@ip-172-31-8-118.ap-northeast-1.compute.internal>
2023-06-26 04:33:54 +0000

    validate

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,5 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :address, uniqueness: true
 end
