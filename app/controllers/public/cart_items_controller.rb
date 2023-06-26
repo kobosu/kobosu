e079df5d43d3a10c4cba4002cafe4ac551ba52b4
ShotaItasaka <shotachemistryt@gmail.com>
2023-06-26 10:48:39 +0000

    編集完了

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,13 +1,19 @@
 class Public::CartItemsController < ApplicationController
   def index
-   @cart_items = CartItem.all
+   @cart_items = current_customer.cart_items
    @total = 0
   end
 
   def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update!(params_cart_item)
+    redirect_to cart_items_path
   end
 
   def destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    redirect_to cart_items
   end
 
   def destroy_all
